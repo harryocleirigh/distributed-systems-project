994f09e40fb0e344e8c1a95a2d02d744531b3b08
harryocleirigh <91558668+harryocleirigh@users.noreply.github.com>
2023-12-22 17:09:08 +0000

    Added logging and error handling for axios POST request to service provider. Also implemented sending the response data back to the front end. Also completely changed folder structure to ensure each microservice type is bundled together

---
+++ application/calculator/app.py
@@ -0,0 +1,128 @@
+import hashlib
+from datetime import datetime
+import json
+from flask_cors import CORS
+from flask import Flask, request, jsonify
+import redis
+
+app = Flask(__name__)
+CORS(app)
+# redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+redis_client = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
+
+
+@app.route('/calculate_credit_score', methods=['POST'])
+def calculate_credit_score():
+    try:
+        data = request.get_json()
+        print("Received data")
+        print(data)
+        # print(" ")
+        
+        user_id = generate_user_id(data['name'], data['dob'])
+
+        # check if user is in cache
+        cached_data = redis_client.get(user_id)
+
+        if cached_data:
+            cached_data = json.loads(cached_data)
+            
+            # cached data will have credit score, have to remove for comparison
+            cached_data_without_credit_score = cached_data.copy()
+            cached_data_without_credit_score.pop('credit_score', None)
+
+            # JSON sent is the same as cached
+            if cached_data_without_credit_score == data:
+                    response_data = {'creditScore': cached_data['credit_score']}
+                    # print("cached data without credit score", cached_data_without_credit_score)
+                    # print("received data", data)
+                    print("************")
+                    print("In cache. Credit score: ", cached_data['credit_score'])
+                    print("************")
+                    # print("*************")
+                    # print(response_data)
+                    # print("*************")
+                    return jsonify(response_data)
+        print("************")
+        print("NOT in cache")
+        print("************")
+        credit_score = calculate_score(data)
+
+        # Store the entire JSON object in the cache
+        data['credit_score'] = credit_score
+        redis_client.set(user_id, json.dumps(data))
+        redis_client.expire(user_id, 3600)
+
+        # just credit score
+        # print("*************")
+        # print(credit_score)
+        # print("*************")
+        return jsonify({'creditScore': credit_score})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+def calculate_score(data):
+    print("Calculating credit score…")
+    dob_date = datetime.strptime(data['dob'], '%Y-%m-%d')
+    today = datetime.now()
+    age = today.year - dob_date.year - ((today.month, today.day) < (dob_date.month, dob_date.day))
+    print("Age: ", age)
+
+    # set base credit score of 600
+    base_score = 600
+
+    # reward being older (over 25) - linear rewards
+    age_factor = max(0, min(100, (age - 25) * 5))
+
+    payment_history = data['missedPaymentsFrequency'].lower()
+    print("missedPaymentsFrequency: ", payment_history)
+    payment_history_factors = {
+        'never': 0,
+        'rarely': -20,
+        'sometimes': -40,
+        'often': -60,
+        'always': -80,
+    }
+
+    # penalisation for missing payments
+    payment_history_factor = payment_history_factors.get(payment_history)
+
+    credit_utilisation = int(data['creditUtilisation'])
+    credit_use_length = int(data['creditUseLength'])
+    debt_to_income_ratio = int(data['debtToIncomeRatio'])
+
+    # penalise higher credit utilisation (over 15)
+    credit_utilisation_penalty = -credit_utilisation * 2 if credit_utilisation > 15 else 0
+    # print('creditUtilisation: ', data['creditUtilisation'])
+    # print("credit utilisation penalty (<15 = 0): ", credit_utilisation_penalty)
+
+    # reward longer credit history - 5 * length of using history
+    credit_history_factor = credit_use_length * 7
+    # print("creditUseLength: ", data['creditUseLength'])
+    # print('credit history reward: ', credit_history_factor)
+
+    # penalise high debt to income ratio
+    debt_to_income_penalty = -debt_to_income_ratio * 3 if debt_to_income_ratio > 20 else 0
+    # print("debtToIncomeRatio: ", data['debtToIncomeRatio'])
+    # print('debt to income penalty (<20 = 0):', debt_to_income_penalty)
+
+    # 50 point deduction if recently applied for credit
+    new_credit_factor = -50 if data['newCredit'].lower() == 'yes' else 0
+    # print("newCredit penalty: ", data['newCredit'])
+
+    total_score = base_score + age_factor + payment_history_factor + credit_utilisation_penalty + \
+        credit_history_factor + debt_to_income_penalty + new_credit_factor
+    
+    print("score at this stage", total_score)
+
+    return max(200, min(1000, total_score))
+
+# generate hash for key in redis - hash of name and age
+def generate_user_id(name, dob):
+    user_data = f"{name.lower()}{dob.lower()}"
+    hashed_user_id = hashlib.sha256(user_data.encode()).hexdigest()
+    return hashed_user_id
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ application/gateway/server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors'); 
+const axios = require('axios');
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+// Middleware for logging
+app.use((req, res, next) => {
+    console.log(`Received request for ${req.url}`);
+    next();
+});
+
+const logger = (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+    console.log('Body:', req.body);
+    next();
+};
+
+app.use(logger);
+
+// Proxy endpoint to Credit Score Generator
+app.use('/api/submit', createProxyMiddleware({ 
+    target: 'http://calculator:5000/calculate_credit_score', // URL of our flask app that will create the credit score for our user
+    // target: 'http://127.0.0.1:5000/calculate_credit_score', // URL of our flask app that will create the credit score for our user
+    changeOrigin: true,
+    pathRewrite: { '^/api/submit': '' },
+    onProxyReq: (proxyReq, req) => {
+        if (req.method === 'POST' && req.body) {
+            let bodyData = JSON.stringify(req.body);
+            proxyReq.setHeader('Content-Type','application/json');
+            proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+            proxyReq.write(bodyData);
+        }
+    },
+    onProxyRes: handleCreditResponse
+}));
+
+function handleCreditResponse(proxyRes, req, res) {
+    let body = [];
+    proxyRes.on('data', (chunk) => {
+        body.push(chunk);
+    });
+    proxyRes.on('end', () => {
+
+        body = Buffer.concat(body).toString();
+        const proxiedResponse = JSON.parse(body);
+        req.body.creditScore = proxiedResponse.creditScore;
+        const completeJSONRes = req.body;
+
+        // console.log("completeJSONRes:", completeJSONRes);
+
+        const serviceProviderType = determineServiceProvider(completeJSONRes);
+
+        // console.log(serviceProviderType)
+
+        const serviceProviderResponse = axios.post(`http://${serviceProviderType}-registry:6000/getQuotes`, completeJSONRes)
+        .then(response => {
+            console.log('Response from service provider:', response.data);
+            return response;
+        })
+        .catch(error => {
+            console.error('Error during axios request:', error);
+        });
+    });
+}
+
+function determineServiceProvider(response) {
+
+    console.log("Response in the determine response", response)
+
+    if (response.loanType == 'student') {
+        return 'student-loan';
+    } else if (response.loanType == 'home') {
+        return 'home-loan';
+    } else if (response.loanType == 'auto') {
+        return 'car-loan';
+    } else if (response.loanType == 'personal') {
+        return 'personal-loan';
+    }
+}
+
+
+
+app.get('/', (req, res) => res.send('API Gateway is running'));
+app.listen(port, () => console.log(`API Gateway listening on port ${port}!`));
